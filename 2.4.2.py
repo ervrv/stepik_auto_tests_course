fc2c160154db8729fc9352dbbd3e4706a7f5f0c4
egor <7877770@gmail.com>
2022-08-02 16:43:03 +0300

    Task 2.4.2

---
+++ 2.4.2.py
@@ -0,0 +1,31 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium import webdriver
+import math
+import time
+
+
+def calc(x):
+    return str(math.log(abs(12 * math.sin(int(x)))))
+
+
+try:
+    link = "http://suninjuly.github.io/explicit_wait2.html"
+    browser = webdriver.Chrome()
+    browser.get(link)
+    browser.find_element(By.ID, "price")
+    WebDriverWait(browser, 12).until(EC.text_to_be_present_in_element((By.ID, "price"), "$100"))
+
+    button = browser.find_element(By.ID, "book")
+    button.click()
+
+    x = browser.find_element(By.CSS_SELECTOR, "#input_value").text
+    y = calc(x)
+
+    browser.find_element(By.CSS_SELECTOR, "#answer").send_keys(y)
+    button = browser.find_element(By.ID, "solve")
+    button.click()
+finally:
+    time.sleep(10)
+    browser.quit()

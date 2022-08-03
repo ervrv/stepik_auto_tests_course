0fe54bc9865c23580f5b77763886e895d175bbbc
egor <7877770@gmail.com>
2022-08-03 15:31:15 +0300

    .

--- course_tasks/pytest_fixtures_3.6.py
+++ course_tasks/pytest_fixtures_3.6.py
@@ -1,19 +1,9 @@
 import pytest
-from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
 import math
-
-
-@pytest.fixture(scope="function")
-def browser():
-    print("\nstart browser for test..")
-    browser = webdriver.Chrome()
-    yield browser
-    print("\nquit browser..")
-    browser.quit()
 
 
 @pytest.mark.parametrize(
